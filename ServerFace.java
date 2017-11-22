abbb85e083dcd653e3151f9b88960210c082b78a
Maslowski Marcin <maslowski.it@gmail.com>
2017-11-22 19:57:45 +0100

    Exaple

---
+++ ServerFace.java
@@ -0,0 +1,17 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerFace
+    extends Remote
+{
+   void CheckLogin(String login, String password) throws RemoteException;
+   void CheckLogout(String login) throws RemoteException;
+
+   void SendTransfer(String fromClient, String toClient, int value) throws RemoteException;
+
+
+   //TESTY
+
+   String getDescription(String text) throws RemoteException;
+
+}
