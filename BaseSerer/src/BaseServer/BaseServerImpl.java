a18b69fbbff09161aeaaf9b6bc49e6335606a2c3
kulisdominik <32802894+kulisdominik@users.noreply.github.com>
2017-12-17 23:35:45 +0100

    fix problem

--- BaseSerer/src/BaseServer/BaseServerImpl.java
+++ BaseSerer/src/BaseServer/BaseServerImpl.java
@@ -13,32 +13,60 @@ public class BaseServerImpl
 
 	// Object logServer print and hold (not yet) information about route on server.
 	// Something like information about client and server errors.
-	LogServer logServer = new LogServer();
+	private LogServer logServer = new LogServer();
 
 	// This object hold every client who is now login on server.
-	ClientSession clientSession = new ClientSession();
+	private ClientSession clientSession = new ClientSession();
 
-	BaseServerFace computingSever;
+	//  This object hold reference to computing servers. Counters represent how much client is connecting to server,
+	private BaseServerFace computingSever_1;
+	private int countComputingSever_1 = 0;
 
-	public BaseServerImpl(BaseServerFace serverFace) throws RemoteException
+	private BaseServerFace computingSever_2;
+	private int countComputingSever_2= 0;
+
+	public BaseServerImpl(BaseServerFace serverFace_1, BaseServerFace serverFace_2) throws RemoteException
 	{
-		this.computingSever = serverFace;
+		this.computingSever_1 = serverFace_1;
+		this.computingSever_2 = serverFace_2;
 	}
 
 	@Override
-	public LogFrom logIn(String login, LogTo data) throws RemoteException
+	public synchronized LogFrom logIn(String login, LogTo data) throws RemoteException
 	{
-		LogFrom user = computingSever.logIn(login, data);
-
-		System.out.println(user);
-
-		if(user.error.equals("0"))
+		synchronized (computingSever_1)
 		{
-			logServer.addLog("Add client " + login + " to session.");
-			clientSession.addClient(login, new ClientData());
+			LogFrom logFrom = computingSever_1.logIn(login, data);
+
+			if(logFrom.error.equals("0"))
+			{
+				// Add error code if client have session now!!!!
+
+				logServer.addMessageToLog("Add client " + login + " to session.");
+
+				ClientData client = new ClientData();
+				clientSession.addClient(login, client);
+
+				if(countComputingSever_2 == countComputingSever_1)
+				{
+					client.setComputingSever(computingSever_2);
+					System.out.println("2: " + countComputingSever_2++);
+				}
+				else if(countComputingSever_2 < countComputingSever_1)
+				{
+					client.setComputingSever(computingSever_2);
+					System.out.println("2: " + countComputingSever_2++);
+				}
+				else
+				{
+					client.setComputingSever(computingSever_1);
+					System.out.println("1: " + countComputingSever_1++);
+				}
+			}
+
+			return logFrom;
 		}
 
-		return user;
 	}
 
 	@Override
@@ -53,14 +81,14 @@ public class BaseServerImpl
 	public String transfer( Transfer data) throws RemoteException
 	{
 		System.out.println("TRANSFER");
-		return computingSever.transfer(data);
+		return computingSever_1.transfer(data);
 	}
 
 	@Override
 	public String changePassword(LogTo data) throws RemoteException
 	{
 		System.out.println("changepassword");
-		return computingSever.changePassword(data);
+		return computingSever_1.changePassword(data);
 
 	}
 
@@ -71,7 +99,7 @@ public class BaseServerImpl
 	}
 
 	@Override
-	public String addFunds(Funds data) throws RemoteException
+	public String addFunds(String login,Funds data) throws RemoteException
 	{
 //		System.out.println("addFunds");
 //		return computingSever.addFunds(data);
@@ -82,7 +110,7 @@ public class BaseServerImpl
 	public String requestAddAccount(String login, PersonalData data) throws RemoteException
 	{
 		System.out.println("requestaddaccount");
-		return  computingSever.requestAddAccount(login, data);
+		return  computingSever_1.requestAddAccount(login, data);
 
 	}
 
@@ -110,7 +138,7 @@ public class BaseServerImpl
 	public String answerAddAccountReq(String login, AddAccReqDecision data) throws RemoteException
 	{
 		System.out.println("ANSWERADDACCOUNTREQ");
-		return computingSever.answerAddAccountReq(login, data);
+		return computingSever_1.answerAddAccountReq(login, data);
 
 	}
 
@@ -136,7 +164,7 @@ public class BaseServerImpl
 	public String getBalance(String login) throws RemoteException
 	{
 		System.out.println("GETBALANCE");
-		return computingSever.getBalance(login);
+		return computingSever_1.getBalance(login);
 	}
 
 	@Override
@@ -167,7 +195,7 @@ public class BaseServerImpl
 	public RequestListAddAccount getRequestAddAccount(String login) throws RemoteException
 	{
 		System.out.println("Zakladanie konta(wniosek)");
-		return computingSever.getRequestAddAccount(login);
+		return computingSever_1.getRequestAddAccount(login);
 	}
 
 	@Override
